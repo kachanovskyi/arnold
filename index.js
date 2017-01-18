a2ed79c1f2d19b9ab82b9e781fa9b414264bd81f
Vova Kachanovskyi <vova.kachanovsky@gmail.com>
2017-01-18 21:27:12 +0200

    added post to form

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 $(document).ready(function () {
+    $('#startForm').on("submit", function (e) {
+        $('#inputBlock').fadeOut(150, function(){
+            var div = $("<div class='success-message'><p>Done! Arnold will contact you shortly</p></div>").hide();
+            $(this).replaceWith(div);
+            $($('.success-message')[0]).fadeIn("slow");
+        });
+        return false;
+    });
+
     $('#start').click(function () {
         $($('.error-message')[0]).hide();
         var number = $('#phone').val();
 
         if(number !== undefined && number != "" && number != " ") {
-            console.log("2. ", number);
 
             if(isNaN(number) || number.match(/\d/g).length !== 10) {
                 $($('.error-message')[0]).fadeIn(100);
                 return;
             } else {
-                $('#inputBlock').fadeOut(150, function(){
-                    var div = $("<div class='success-message'><p>Done! Arnold will contact you shortly</p></div>").hide();
-                    $(this).replaceWith(div);
-                    $($('.success-message')[0]).fadeIn("slow");
-                });
+                $('#startForm').submit();
+
             }
         } else {
             $($('.error-message')[0]).fadeIn(100);
         }
-
     });
 });
