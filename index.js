80bc3c0c74b126865fb41ec6fe9ae2e98688a5a5
Vova Kachanovskyi <vova.kachanovsky@gmail.com>
2017-01-20 14:17:50 +0200

    fixed form redirection

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 $(document).ready(function () {
+    var number;
+
     $('#startForm').on("submit", function (e) {
         $('#inputBlock').fadeOut(150, function(){
             var div = $("<div class='success-message'><p>Done! Arnold will contact you shortly</p></div>").hide();
             $(this).replaceWith(div);
             $($('.success-message')[0]).fadeIn("slow");
         });
-        // return false;
     });
 
-    $('#start').click(function () {
+    $('#start').click(function (e) {
         $($('.error-message')[0]).hide();
-        var number = $('#phone').val();
+        number = $('#phone').val();
 
         if(number !== undefined && number != "" && number != " ") {
-            console.log($('#startForm'));
-
             if(isNaN(number)) {
                 $($('.error-message')[0]).fadeIn(100);
                 return;
             } else {
                 $('#startForm').submit();
-
             }
         } else {
             $($('.error-message')[0]).fadeIn(100);
         }
+    });
+
+    $('#startForm').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            // url: 'http://requestb.in/t29bn7t2',
+            url: 'https://fitness.botscrew.com/add',
+            data: {data: number},
+            type: 'POST',
+            crossDomain: true,
+            dataType: 'jsonp'
+            // success: function() { console.log("Success"); },
+            // error: function() { console.log('Failed!'); }
+        });
     });
 });
