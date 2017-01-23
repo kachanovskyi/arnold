4e6c44688296902af67d33b3a888f54259d1847f
Vova Kachanovskyi <vova.kachanovsky@gmail.com>
2017-01-23 18:17:12 +0200

    changed form submit parameters to previous form for testing

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
+var submitForm;
+
 $(document).ready(function () {
     var number;
 
-    $('#startForm').on("submit", function (e) {
-        $('#inputBlock').fadeOut(150, function(){
-            var div = $("<div class='success-message'><p>Done! Arnold will contact you shortly</p></div>").hide();
-            $(this).replaceWith(div);
-            $($('.success-message')[0]).fadeIn("slow");
-        });
-    });
-
-    $('#start').click(function (e) {
+    submitForm = function() {
         $($('.error-message')[0]).hide();
+
         number = $('#phone').val();
 
         if(number !== undefined && number != "" && number != " ") {
@@ -18,24 +13,66 @@ $(document).ready(function () {
                 $($('.error-message')[0]).fadeIn(100);
                 return;
             } else {
-                $('#startForm').submit();
+                // e.preventDefault();
+                // $('#startForm').submit();
+                $('#inputBlock').fadeOut(150, function(){
+                    var div = $("<div class='success-message'><p>Done! Arnold will contact you shortly</p></div>").hide();
+                    $(this).replaceWith(div);
+                    $($('.success-message')[0]).fadeIn("slow");
+                });
+
+                $.ajax({
+                    // url: 'http://requestb.in/16jnqk41',
+                    url: 'https://fitness.botscrew.com/add',
+                    data: { phoneNumber: number },
+                    type: 'POST',
+                    method: 'POST',
+                    crossDomain: true,
+                    dataType: 'jsonp',
+                    success: function() { console.log("Success"); },
+                    error: function() { console.log('Failed!'); }
+                });
             }
         } else {
             $($('.error-message')[0]).fadeIn(100);
         }
-    });
+    }
 
-    $('#startForm').submit(function(e){
-        e.preventDefault();
-        $.ajax({
-            // url: 'http://requestb.in/15h1yjz1',
-            url: 'https://fitness.botscrew.com/add',
-            data: { phoneNumber: number },
-            type: 'POST',
-            crossDomain: true,
-            dataType: 'jsonp'
-            // success: function() { console.log("Success"); },
-            // error: function() { console.log('Failed!'); }
-        });
-    });
+    // $('#startForm').on("submit", function (e) {
+    //     $('#inputBlock').fadeOut(150, function(){
+    //         var div = $("<div class='success-message'><p>Done! Arnold will contact you shortly</p></div>").hide();
+    //         $(this).replaceWith(div);
+    //         $($('.success-message')[0]).fadeIn("slow");
+    //     });
+    // });
+
+    // $('#start').click(function (e) {
+    //     $($('.error-message')[0]).hide();
+    //     number = $('#phone').val();
+    //
+    //     if(number !== undefined && number != "" && number != " ") {
+    //         if(isNaN(number)) {
+    //             $($('.error-message')[0]).fadeIn(100);
+    //             return;
+    //         } else {
+    //             $('#startForm').submit();
+    //         }
+    //     } else {
+    //         $($('.error-message')[0]).fadeIn(100);
+    //     }
+    // });
+
+    // $('#startForm').submit(function(e){
+    //     e.preventDefault();
+    //     $.ajax({
+    //         // url: 'http://requestb.in/15h1yjz1',
+    //         url: 'https://fitness.botscrew.com/add',
+    //         data: { phoneNumber: number },
+    //         method: 'POST',
+    //         crossDomain: true,
+    //         dataType: 'jsonp'
+    //         // success: function() { console.log("Success"); },
+    //         // error: function() { console.log('Failed!'); }
+    //     });
+    // });
 });
