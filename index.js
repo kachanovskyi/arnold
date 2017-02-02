4312e569876de1fcd3f01abdc19fe2f9ab4a58f0
Vova Kachanovskyi <vova.kachanovsky@gmail.com>
2017-02-02 17:05:13 +0200

    implemented new landing design. Mobile version to be fixed

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 $(document).ready(function () {
     $('<iframe name="mock_frame" style="display: none; visibility: hidden;"></iframe>').appendTo('body');
 
-    $('#startForm').on("submit", function (e) {
-        $('#inputBlock').fadeOut(150, function(){
+    $('#startTopForm, #startBottomForm').on("submit", function () {
+        var input = $($('.inputBlock')[0]);
+
+        if($(this).attr('id') === 'startBottomForm') {
+            input = $($('.inputBlock')[1]);
+            console.log(33);
+        }
+
+        input.fadeOut(150, function(){
             var div = $("<div class='success-message'><p>Done! Arnold will contact you shortly</p></div>").hide();
             $(this).replaceWith(div);
             $($('.success-message')[0]).fadeIn("slow");
         });
+
     });
 
-    $('#start').click(function (e) {
+    $('#startTop').click(startBtnAction);
+    $('#startBottom').click(startBtnAction);
+
+    function startBtnAction() {
+        var number = $('#phoneTop').val();
+        var id = $(this).attr('id');
         var errorMessage = $($('.error-message')[0]);
 
+        if(id === 'startBottom') {
+            console.log('window');
+            errorMessage = $($('.error-message')[1]);
+            number = $('#phoneBottom').val();
+        }
+        console.log(errorMessage);
         errorMessage.hide();
-        number = $('#phone').val();
 
         if(number !== undefined && number != "" && number != " ") {
             if(isNaN(number)) {
                 errorMessage.fadeIn(100);
                 return;
-            } else {
-                $('#startForm').submit();
+            } else if(id === 'startTop') {
+                $('#startTopForm').submit();
+                console.log('submitting');
+            } else if(id === 'startBottom') {
+                $('#startBottomForm').submit();
+                console.log('submitting bottom');
             }
         } else {
             errorMessage.fadeIn(100);
         }
-    });
+    }
 });
