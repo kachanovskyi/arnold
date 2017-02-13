d6137905470020f3bbdc2826a98ecb54e8bdbca7
Vova Kachanovskyi <vova.kachanovsky@gmail.com>
2017-02-13 16:12:32 +0200

    added dynamic text

--- js/index.js
+++ js/index.js
@@ -48,4 +48,17 @@ $(document).ready(function () {
             errorMessage.fadeIn(100);
         }
     }
+
+    $('a.page-scroll').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 900);
+                return false;
+            }
+        }
+    });
 });
